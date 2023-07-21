c5edd05d7088ac0bd587cd49744dfa0fc90732db
Pablo Androetto <pablo.androetto@wearepsh.com>
2023-07-21 01:25:15 -0300

    debug things

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { globalCss } from "./theme";
 
 const globalStyles = globalCss({
     body: { margin: 0, fontFamily: "$default" },
 });
 
+declare const chrome: any;
+
 function App() {
+    const [apiKey, _setApiKey] = useState(
+        localStorage.getItem("OPENAI_API_KEY") || "",
+    );
+    const setApiKey = (apiKey: string) => {
+        localStorage.setItem("OPENAI_API_KEY", apiKey);
+        _setApiKey(apiKey);
+    };
+
     globalStyles();
-    return <div>test</div>;
+    return (
+        <div>
+            <label>
+                openai api key <br />
+                <input
+                    type="text"
+                    value={apiKey}
+                    onChange={(e) => setApiKey(e.target.value)}
+                />
+                <button onClick={() => {
+                    chrome.storage.local.get("state").then((e:any) => console.log(e))
+                }}> asd</button>
+            </label>
+        </div>
+    );
 }
 
 export default App;
