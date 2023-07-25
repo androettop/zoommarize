c8eac78527b1aca838657cff58ebb2c22b46cdf0
Juan Manuel Androetto <juan.androetto36@gmail.com>
2023-07-25 16:57:32 -0300

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { StorageProvider } from "./helpers/storage";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <StorageProvider>
-            <App />
+            <BrowserRouter>
+                 <App />
+            </BrowserRouter>
         </StorageProvider>
     </React.StrictMode>,
 );
